3df31a866b5995884cec0c5682bd7398b03ca130
Max <maxbaldino@aol.com>
2020-07-17 19:58:30 -0400

    Isopod spawn egg

--- src/main/java/com/luminousdepths/luminousdepthsmod/registries/LuminousItems.java
+++ src/main/java/com/luminousdepths/luminousdepthsmod/registries/LuminousItems.java
@@ -3,10 +3,13 @@ package com.luminousdepths.luminousdepthsmod.registries;
 import com.luminousdepths.luminousdepthsmod.Blocks.BlockItemBase;
 import com.luminousdepths.luminousdepthsmod.Blocks.ItemBase;
 import com.luminousdepths.luminousdepthsmod.LuminousDepthsMod;
+import com.luminousdepths.luminousdepthsmod.entities.IsopodEntity;
 import com.luminousdepths.luminousdepthsmod.liquids.Brine;
+import net.minecraft.entity.EntityType;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -21,6 +24,10 @@ public class LuminousItems {
     public static final RegistryObject<Item> ESCA = ITEMS.register("esca", () -> new ItemBase(new Item.Properties().group(ItemGroup.MATERIALS)));
     public static final RegistryObject<Item> ISOCARAPACE = ITEMS.register("isocarapace", () -> new ItemBase(new Item.Properties().group(ItemGroup.MATERIALS)));
     public static final RegistryObject<Item> CORALLINE_ALGAE_GROWTH = ITEMS.register("coralline_algae_growth", () -> new ItemBase(new Item.Properties().group(ItemGroup.MATERIALS)));
+
+    public static final RegistryObject<Item> ISOPOD_SPAWN_EGG = ITEMS.register("coralline_algae_growth", () -> new SpawnEggItem(LuminousEntities.ISOPOD.get(),11022961, 11035249, new Item.Properties().group(ItemGroup.MATERIALS)));
+
+
 
     public static void init() {
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
